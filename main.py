db035fef032113b7b41ba58db4cc85653acdcea0
Flyon72 <82049112+Flyon72@users.noreply.github.com>
2021-04-08 14:09:15 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ bot = commands.Bot(command_prefix='!')
 async def code(ctx):
     await ctx.send('testowa wiadomość')
 
-@bot.command(pass_context=True)
-@commands.has_role("Admin") # This must be exactly the name of the appropriate role
-async def addrole(ctx):
-    member = ctx.message.author
-    role = get(member.server.roles, name="Test")
-    await bot.add_roles(member, role)
 
-bot.run('ODI4OTQxMTM4NTgwODY1MDM0.YGw5mA.umuG2LnYjeLhKX4GAIpk7Tda8QQ')
+
+
